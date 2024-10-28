fa02d2f10214b08f3b0d1da4933b6c4ee8eb8530
denielle <dennj644@gmail.com>
2024-10-28 13:43:14 +0800

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <section className="#Home">
+        <div className="home-content">
+            <div className="left">   
+                <h3>Welcome to My Portfolio</h3>
+                    <h1>I'm Denielle Derecho Jayme,</h1>
+                <p>
+                    Intermediate Front-end Developer,
+                    Information Technology Student of <br />
+                    Western Institute of Technology
+                </p>
+                <button className="contact-button">
+                    <a href="#Contact">Contact Me</a>
+                </button>
+            </div>
+            <div className="right">
+                <img src="bodybuilder.jpeg" alt="Denielle" width='400px' border-radius='50%'/>
+                
+            </div>            
+        </div>
+    </section>
+  );
+}
+
+export default Home;
